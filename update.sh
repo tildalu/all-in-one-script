51be7a54535bae939af7a474b5ef952621ed1306
Tilda <tilda.lu@trunk-studio.com>
2021-06-03 17:54:26 +0800

    python3 update

--- update.sh
+++ update.sh
@@ -1,3 +1,23 @@
+#!/bin/zsh
+# To execute run:
+#
+#    zsh update-all.sh
+#
+# To source and then use individual update-* functions
+# first comment out the command at the bottom of the file
+# and run:
+#
+#    source ./update-all.sh
+#
+# If you want to use this command often copy it to directory
+# that you have in PATH (check with `echo $PATH`) like this:
+#
+#     USER_SCRIPTS="${HOME}/.local/bin"  # change this
+#     cp ./update-all.sh $USER_SCRIPTS/update-all
+#     chmod +x $USER_SCRIPTS/update-all
+#
+#  and now you can call the script any time :)
+
 # Text Color Variables
 GREEN='\033[32m' # Green
 CLEAR='\033[0m'  # Clear color and formatting
@@ -20,14 +40,12 @@ update-brew() {
     brew missing
 }
 
-
 update-npm() {
     if ! which npm &>/dev/null; then return; fi
 
     echo -e "\n${GREEN}Updating npm Packages${CLEAR}"
     npm update -g
 }
-
 
 update-yarn() {
     if ! which yarn &>/dev/null; then return; fi
@@ -50,7 +68,8 @@ update-pip3() {
     if ! which python3 &>/dev/null; then return; fi
 
     echo -e "\n${GREEN}Updating Python 3.X pips${CLEAR}"
-    python3 -c "import pkg_resources; from subprocess import call; packages = [dist.project_name for dist in pkg_resources.working_set]; call('pip install --upgrade ' + ' '.join(packages), shell=True)"
+    /usr/local/opt/python@3.9/bin/python3.9 -m pip install --upgrade pip
+    # python3 -c "import pkg_resources; from subprocess import call; packages = [dist.project_name for dist in pkg_resources.working_set]; call('pip install --upgrade ' + ' '.join(packages), shell=True)"
     #pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U
 }
 
