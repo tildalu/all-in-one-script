9b3708852cc3d8306530d09ba398cba27d53065e
jimchen5209 <jimchen5209@gmail.com>
2022-11-11 10:03:36 +0800

    fix: missing "

--- install.sh
+++ install.sh
@@ -215,7 +215,7 @@ install-node() {
     corepack enable
 
     echo -e "\n${YELLOW}${BOLD}STEP ${BLUE}=> ${WHITE}Add nvm config to zshrc${CLEAR}"
-    echo "export NVM_DIR=\"\$([ -z \"\${XDG_CONFIG_HOME-}\" ] && printf %s \"\${HOME}/.nvm\" || printf %s \"\${XDG_CONFIG_HOME}/nvm\")" >> ~/.zshrc
+    echo "export NVM_DIR=\"\$([ -z \"\${XDG_CONFIG_HOME-}\" ] && printf %s \"\${HOME}/.nvm\" || printf %s \"\${XDG_CONFIG_HOME}/nvm\")\"" >> ~/.zshrc
     echo "[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\" # This loads nvm" >> ~/.zshrc
     echo "" >> ~/.zshrc
 
