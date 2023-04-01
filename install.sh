b253289b083f0d430be4a13b9a528d6b71a17ed6
tilda.lu <lu23770127@gmail.com>
2023-04-02 02:59:40 +0800

    feat(test): 🎨 add line between nvm script
    separate makes more easy to read

--- install.sh
+++ install.sh
@@ -93,8 +93,10 @@ install-dev-software() {
     # NVM
     echo -e "${YELLOW}Install NVM${CLEAR}"
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
     source ~/.nvm/nvm.sh
 
     # nginx
